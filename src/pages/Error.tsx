6c703580d05eb27a8a30367ab794c2ef51ad95e8
Harry <24208175+manister@users.noreply.github.com>
2022-10-27 21:33:15 +0100

    error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,16 +1,12 @@
-import { Link, useNavigate, useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 import Container from "../components/layout/Container";
 
 const ErrorPage = () => {
   const error = useRouteError() as Error;
-  const navigate = useNavigate();
   return (
     <Container>
       <Link to="/">Home page</Link>
-      <button type="button" onClick={() => navigate(-1)}>
-        Back
-      </button>
-      <h1>{error.message}</h1>
+      <h1>{error.message ?? "There has been an error"}</h1>
     </Container>
   );
 };
