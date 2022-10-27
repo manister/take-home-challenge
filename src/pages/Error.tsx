995dc7b4bde7823fb2146b5b68f17ba8dbf625a6
Harry <24208175+manister@users.noreply.github.com>
2022-10-27 21:34:30 +0100

    error page improvement

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,12 +1,18 @@
 import { Link, useRouteError } from "react-router-dom";
+import PageTitle from "../components/elements/PageTitle";
 import Container from "../components/layout/Container";
 
 const ErrorPage = () => {
   const error = useRouteError() as Error;
   return (
     <Container>
-      <Link to="/">Home page</Link>
-      <h1>{error.message ?? "There has been an error"}</h1>
+      <PageTitle>{error.message ?? "There has been an error"}</PageTitle>
+      <p className="text-center">
+        Back to{" "}
+        <Link className="underline" to="/">
+          home page
+        </Link>
+      </p>
     </Container>
   );
 };
